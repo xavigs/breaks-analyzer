7b458bbc37e6d9ddade21532c204222a18e2355b
Xavi G. Sunyer <xaviergs1984@gmail.com>
2021-08-10 07:30:51 +0200

    Get day parameter

--- breakAnalysis.py
+++ breakAnalysis.py
@@ -1,4 +1,13 @@
+import sys
 import flashScore
 
-url = flashScore.getDailyGames()
-print(url)
+if len(sys.argv) == 2:
+    if sys.argv[1] != "today" and sys.argv[1] != "tomorrow":
+        print("ERROR: El paràmetre passat és incorrecte. S'espera el dia a analitzar (today o tomorrow)")
+    else:
+        day = sys.argv[1]
+        print(day)
+        exit()
+        url = flashScore.getDailyGames(day)
+else:
+    print("ERROR: El nombre de paràmetres és incorrecte.")
--- flashScore.py
+++ flashScore.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-def getDailyGames(day="today"):
+def getDailyGames(day):
     # Load FlashScore WS with daily matches
     if day == "today":
         url = "https://d.flashscore.es/x/feed/f_2_0_1_es_1"
