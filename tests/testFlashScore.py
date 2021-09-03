6e90402794cfc9a01d5babda1218bc8ed25bf802
Xavi G. Sunyer <xaviergs1984@gmail.com>
2021-09-03 07:25:37 +0200

    Test get daily games

--- tests/testFlashScore.py
+++ tests/testFlashScore.py
@@ -1,9 +1,18 @@
 import unittest
+from flashScore import *
 
 class TestFlashScore(unittest.TestCase):
 
     def test_get_daily_games(self):
-        self.assertEqual(3, 3)
+        # Test for today
+        games = getDailyGames("today")
+        self.assertIsNotNone(games)
+        self.assertIsInstance(games, list)
+
+        # Test for tomorrow
+        games = getDailyGames("tomorrow")
+        self.assertIsNotNone(games)
+        self.assertIsInstance(games, list)
 
 if __name__ == "__main__":
     unittest.main()
