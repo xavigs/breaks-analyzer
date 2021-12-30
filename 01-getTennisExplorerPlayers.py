d4c585d6a2052bc612c9361447871c18a3f60f96
juxtecorp <xaviergs1984@gmail.com>
2021-12-30 20:23:17 +0100

    Get player data from Tennis Explorer ranking

---
+++ 01-getTennisExplorerPlayers.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import sys
+import requests
+from bs4 import BeautifulSoup
+sys.path.insert(1, 'models')
+import db, objects
+
+categories = [{'sex': 'M', 'URL_BASE': "https://www.tennisexplorer.com/ranking/atp-men/?page="},
+              {'sex': 'W', 'URL_BASE': "https://www.tennisexplorer.com/ranking/wta-women/?page="}]
+
+dbConnection = db.Database()
+breaksDB = dbConnection.connect()
+playersObj = objects.Players(breaksDB)
+playersObj.empty() # To delete
+
+for category in categories:
+    page = 1
+    ranking = 1
+    end = False
+
+    while not end:
+        print("# Extracting players from the page " + str(page))
+        url = category['URL_BASE'] + str(page)
+        r = requests.get(url)
+        data = r.text
+        soup = BeautifulSoup(data, "lxml")
+        players = soup.select("tbody[class=flags] tr")
+        
+        if len(players) == 0:
+            end = True
+            continue
+        else:
+            for player in players:
+                playerDB = {}
+                tennisExplorerKeyword = player.select("td[class=t-name] a")[0]['href'].split("/")[2]
+                playerDB['_id'] = tennisExplorerKeyword
+                playerDB['sex'] = category['sex']
+                playerDB['startingRanking'] = ranking
+                playerDB['tennisExplorerKeyword'] = tennisExplorerKeyword
+                playerDB['tennisExplorerName'] = player.select("td[class=t-name]")[0].text
+                playersObj.write(playerDB)
+                ranking += 1
+
+        page += 1
+
+dbConnection.close()
