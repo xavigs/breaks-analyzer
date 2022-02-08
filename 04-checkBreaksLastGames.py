fe2c58b41077663998eb3c1acc55b9042428c9cc
juxtecorp <xaviergs1984@gmail.com>
2022-02-08 18:38:15 +0100

    Error message when opponent is not found

--- 04-checkBreaksLastGames.py
+++ 04-checkBreaksLastGames.py
@@ -8,10 +8,10 @@ dbConnection = db.Database()
 breaksDB = dbConnection.connect()
 playersObj = objects.Players(breaksDB)
 
-#players = playersObj.read()
-players = playersObj.getWomen()
+players = playersObj.read()
+#players = playersObj.getWomen()
 
-for player in players[62:100]:
+for player in players[135:200]:
     rankingNameLength = len(str(player['startingRanking'])) + len(player['tennisExplorerName'])
     print("\n" + "-" * (rankingNameLength + 25))
     print("|          ({}) {}          |".format(player['startingRanking'], player['tennisExplorerName'].upper()))
@@ -20,9 +20,15 @@ for player in players[62:100]:
 
     for game in player['lastGames']:
         previousGame = {}
-        previousGame['opponent'] = playersObj.find([{'_id': game['opponent']}])['flashScoreId']
-        previousGame['date'] = game['time']
-        lastGames.append(previousGame)
+        opponent = playersObj.find([{'_id': game['opponent']}])
+
+        if opponent is None:
+            print("❌ The opponent {} is not into the database.".format(game['opponent']))
+            exit()
+        else:
+            previousGame['opponent'] = opponent['flashScoreId']
+            previousGame['date'] = game['time']
+            lastGames.append(previousGame)
     
     lastGamesBreaks = flashScore.checkBreaksLastGamesByPlayer(player['flashScoreId'], player['flashScoreName'], lastGames)
     playersObj.updateBreakData(player['_id'], lastGamesBreaks)
