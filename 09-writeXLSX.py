3c705a20b728925b85154681f119eac30b55318b
xavigs <xaviergs1984@gmail.com>
2022-02-11 10:01:29 +0100

    Create XLSX with real data

--- 09-writeXLSX.py
+++ 09-writeXLSX.py
@@ -8,6 +8,8 @@ from models import db, objects
 dbConnection = db.Database()
 breaksDB = dbConnection.connect()
 gamesObj = objects.Games(breaksDB)
+tournamentsObj = objects.Tournaments(breaksDB)
+playersObj = objects.Players(breaksDB)
 dayDateTime = date.today()
 dayString = dayDateTime.strftime("%Y-%m-%d")
 filepath = "xlsx/{}.xlsx".format(dayString)
@@ -44,6 +46,17 @@ alignmentCenter = Alignment(
 rightBorder = Border(
     right = Side(style="thin")
 )
+categories = {
+    'ATP-250': {'backColor': "1eaeea", 'foreFont': fontRegularWhite},
+    'ATP-500': {'backColor': "ccccdd", 'foreFont': fontRegularBlack},
+    'ATP-1000': {'backColor': "bb9640", 'foreFont': fontRegularWhite},
+    'ATP-GS': {'backColor': "20305a", 'foreFont': fontRegularWhite},
+    'CH': {'backColor': "53a01d", 'foreFont': fontRegularWhite},
+    'ITF': {'backColor': "f8c408", 'foreFont': fontRegularBlack},
+    'WTA': {'backColor': "7814ff", 'foreFont': fontRegularWhite}
+}
+categoriesToCombine = ["CH", "ITF"]
+surfaces = {'D': "788575", 'T': "ff7500", 'H': "74ae30", 'I': "618cb1"}
 
 worksheet = workbook.active
 worksheet.title = "Daily games"
@@ -57,110 +70,101 @@ games = gamesObj.find_all([{'gameDay': dayString, 'odd': {"$exists": True, "$gte
 
 for gameIndex, game in enumerate(games):
     numRow = gameIndex + 1
-    worksheet['B{}'.format(numRow)] = "ATP"
-    worksheet['B{}'.format(numRow)].font = fontRegularBlack
+    tournament = tournamentsObj.find([{'_id': game['tournament']}])
+    player1 = playersObj.find([{'_id': game['player1ID']}])
+    player2 = playersObj.find([{'_id': game['player2ID']}])
+
+    if tournament['category'] == "ATP":
+        categoryKey = "ATP-{}".format(tournament['subcategory'])
+    else:
+        categoryKey = tournament['category']
+
+    worksheet['B{}'.format(numRow)] = tournament['category']
+    worksheet['B{}'.format(numRow)].font = categories[categoryKey]['foreFont']
     worksheet['B{}'.format(numRow)].alignment = alignmentCenter
-    worksheet['B{}'.format(numRow)].fill = PatternFill(start_color = "ccccdd", end_color = "ccccdd", fill_type = "solid")
-    worksheet['C{}'.format(numRow)] = 500
-    worksheet['C{}'.format(numRow)].font = fontRegularBlack
-    worksheet['C{}'.format(numRow)].alignment = alignmentCenter
-    worksheet['C{}'.format(numRow)].fill = PatternFill(start_color = "ccccdd", end_color = "ccccdd", fill_type = "solid")
-    worksheet['D{}'.format(numRow)] = "I"
+    worksheet['B{}'.format(numRow)].fill = PatternFill(start_color = categories[categoryKey]['backColor'], end_color = categories[categoryKey]['backColor'], fill_type = "solid")
+
+    if tournament['category'] in categoriesToCombine:
+        worksheet.merge_cells('B{}:C{}'.format(numRow, numRow))
+    else:
+        worksheet['C{}'.format(numRow)] = tournament['subcategory']
+        worksheet['C{}'.format(numRow)].font = categories[categoryKey]['foreFont']
+        worksheet['C{}'.format(numRow)].alignment = alignmentCenter
+        worksheet['C{}'.format(numRow)].fill = PatternFill(start_color = categories[categoryKey]['backColor'], end_color = categories[categoryKey]['backColor'], fill_type = "solid")
+
+    worksheet['D{}'.format(numRow)] = tournament['surface']
     worksheet['D{}'.format(numRow)].font = fontRegularWhite
     worksheet['D{}'.format(numRow)].alignment = alignmentCenter
-    worksheet['D{}'.format(numRow)].fill = PatternFill(start_color = "618cb1", end_color = "618cb1", fill_type = "solid")
-    worksheet['E{}'.format(numRow)] = "Text1"
+    worksheet['D{}'.format(numRow)].fill = PatternFill(start_color = surfaces[tournament['surface']], end_color = surfaces[tournament['surface']], fill_type = "solid")
+    worksheet['E{}'.format(numRow)] = " ".join(game['FS-player1'].split(" ")[0:-1])
     worksheet['E{}'.format(numRow)].font = fontBoldBlack
     worksheet['E{}'.format(numRow)].alignment = alignmentLeft
-    worksheet['F{}'.format(numRow)] = "Text2"
+    worksheet['F{}'.format(numRow)] = " ".join(game['FS-player2'].split(" ")[0:-1])
     worksheet['F{}'.format(numRow)].font = fontRegularBlack
     worksheet['F{}'.format(numRow)].alignment = alignmentLeft
     worksheet['G{}'.format(numRow)] = "OK"
     worksheet['G{}'.format(numRow)].font = fontRegularWhite
     worksheet['G{}'.format(numRow)].alignment = alignmentCenter
     worksheet['G{}'.format(numRow)].fill = PatternFill(start_color = "34a853", end_color = "34a853", fill_type = "solid")
-    worksheet['H{}'.format(numRow)] = 1.61
+    worksheet['H{}'.format(numRow)] = 1.01
     worksheet['H{}'.format(numRow)].font = fontRegularBlack
     worksheet['H{}'.format(numRow)].alignment = alignmentCenter
-    worksheet['I{}'.format(numRow)] = "N"
-    worksheet['I{}'.format(numRow)].font = fontRegularWhite
+
+    if player1['country'] == tournament['country']:
+        player1['home'] = "S"
+    else:
+        player1['home'] = "N"
+
+    if player2['country'] == tournament['country']:
+        player2['home'] = "S"
+    else:
+        player2['home'] = "N"
+
+    if player1['home'] == player2['home']:
+        backColor = "ffffff"
+        foreFont = fontRegularBlack
+    elif player1['home'] == "S":
+        backColor = "34a853"
+        foreFont = fontRegularWhite
+    else:
+        backColor = "ee0000"
+        foreFont = fontRegularWhite
+
+    worksheet['I{}'.format(numRow)] = player1['home']
+    worksheet['I{}'.format(numRow)].font = foreFont
     worksheet['I{}'.format(numRow)].alignment = alignmentCenter
-    worksheet['I{}'.format(numRow)].fill = PatternFill(start_color = "ee0000", end_color = "ee0000", fill_type = "solid")
-    worksheet['J{}'.format(numRow)] = "S"
-    worksheet['J{}'.format(numRow)].font = fontRegularWhite
+    worksheet['J{}'.format(numRow)] = player2['home']
+    worksheet['J{}'.format(numRow)].font = foreFont
     worksheet['J{}'.format(numRow)].alignment = alignmentCenter
-    worksheet['J{}'.format(numRow)].fill = PatternFill(start_color = "ee0000", end_color = "ee0000", fill_type = "solid")
-    worksheet['K{}'.format(numRow)] = 1.66
+
+    if backColor != "ffffff":
+        worksheet['I{}'.format(numRow)].fill = PatternFill(start_color = backColor, end_color = backColor, fill_type = "solid")
+        worksheet['J{}'.format(numRow)].fill = PatternFill(start_color = backColor, end_color = backColor, fill_type = "solid")
+
+    worksheet['K{}'.format(numRow)] = game['odd']
     worksheet['K{}'.format(numRow)].font = fontRegularBlack
     worksheet['K{}'.format(numRow)].alignment = alignmentCenter
-    worksheet['L{}'.format(numRow)].value = "74,50%"
+    worksheet['K{}'.format(numRow)].number_format = "#,##0.00"
+    worksheet['L{}'.format(numRow)].value = str(game['probability']).replace(".", ",") + "%"
     worksheet['L{}'.format(numRow)].number_format = "0.00%"
     worksheet['L{}'.format(numRow)].font = fontBoldBlack
     worksheet['L{}'.format(numRow)].alignment = alignmentCenter
-    worksheet['M{}'.format(numRow)].value = "=1/K1" # 62,11%
+    worksheet['M{}'.format(numRow)].value = "=1/K{}".format(numRow) # 62,11%
     worksheet['M{}'.format(numRow)].number_format = "0.00%"
     worksheet['M{}'.format(numRow)].font = fontRegularBlack
     worksheet['M{}'.format(numRow)].alignment = alignmentCenter
-    worksheet['N{}'.format(numRow)] = "=L1/M1"
+    worksheet['N{}'.format(numRow)] = "=L{}/M{}".format(numRow, numRow)
     worksheet['N{}'.format(numRow)].font = fontBoldBlack
     worksheet['N{}'.format(numRow)].alignment = alignmentCenter
     worksheet['N{}'.format(numRow)].number_format = "#,##0.00"
-    worksheet['B{}'.format(numRow)].border = rightBorder
-    
-    '''worksheet['B2'] = "CH"
-    worksheet['B2'].font = fontRegularWhite
-    worksheet['B2'].alignment = alignmentCenter
-    worksheet['B2'].fill = PatternFill(start_color = "53a01d", end_color = "53a01d", fill_type = "solid")
-    worksheet.merge_cells('B2:C2')
-    worksheet['D2'] = "D"
-    worksheet['D2'].font = fontRegularWhite
-    worksheet['D2'].alignment = alignmentCenter
-    worksheet['D2'].fill = PatternFill(start_color = "788575", end_color = "788575", fill_type = "solid")
-    worksheet['E2'] = "Text3"
-    worksheet['E2'].font = fontBoldBlack
-    worksheet['E2'].alignment = alignmentLeft
-    worksheet['F2'] = "Text4"
-    worksheet['F2'].font = fontRegularBlack
-    worksheet['F2'].alignment = alignmentLeft
-    worksheet['G2'] = "10"
-    worksheet['G2'].font = fontRegularBlack
-    worksheet['G2'].alignment = alignmentCenter
-    worksheet['H2'] = 1.61
-    worksheet['H2'].font = fontRegularBlack
-    worksheet['H2'].alignment = alignmentCenter
-    worksheet['I2'] = "S"
-    worksheet['I2'].font = fontRegularWhite
-    worksheet['I2'].alignment = alignmentCenter
-    worksheet['I2'].fill = PatternFill(start_color = "34a853", end_color = "34a853", fill_type = "solid")
-    worksheet['J2'] = "N"
-    worksheet['J2'].font = fontRegularWhite
-    worksheet['J2'].alignment = alignmentCenter
-    worksheet['J2'].fill = PatternFill(start_color = "34a853", end_color = "34a853", fill_type = "solid")
-    worksheet['K2'] = 1.66
-    worksheet['K2'].font = fontRegularBlack
-    worksheet['K2'].alignment = alignmentCenter
-    worksheet['L2'].value = "74,50%"
-    worksheet['L2'].number_format = "0.00%"
-    worksheet['L2'].font = fontBoldBlack
-    worksheet['L2'].alignment = alignmentCenter
-    worksheet['M2'].value = "=1/K1" # 62,11%
-    worksheet['M2'].number_format = "0.00%"
-    worksheet['M2'].font = fontRegularBlack
-    worksheet['M2'].alignment = alignmentCenter
-    worksheet['N2'] = "=L1/M1"
-    worksheet['N2'].font = fontBoldBlack
-    worksheet['N2'].alignment = alignmentCenter
-    worksheet['N2'].number_format = "#,##0.00"'''
 
-worksheet.merge_cells('A1:A2')
+worksheet.merge_cells('A1:A{}'.format(numRow))
 workbook.save(filepath)
 
 '''
-    # TODO DB
+    # TODO
 
-    1) Collection tournament (teID)
-    2) Save tournament data: teID (_id), teName, sex, category, surface
-    3) Into game documents, new fields: tournament (teID), time
-    4) Into player lastGames (or game documents), newField: last ("OK", "01", "02", "10", "11", "12", "20", "21", "22")
-    5) Tractament d'excepcions a tots els scripts, perquè es generi un log, però no deixi de funcionar quan un element falla
+    1) Into player lastGames (or game documents), newField: last ("OK", "01", "02", "10", "11", "12", "20", "21", "22")
+    2) Opponent win odd
+    3) Print avgProbability in 07-printDailyGames
 '''
