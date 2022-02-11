e7009bc28377167961e914f905a9baf763dd965d
juxtecorp <xaviergs1984@gmail.com>
2022-02-11 16:04:33 +0100

    Get SofaScore data!

--- sofaScore.py
+++ sofaScore.py
@@ -1,45 +1,30 @@
 # -*- coding: utf-8 -*-
 import requests
 import json
+import brotli
 from utils import *
 
 BASE_URL = "https://api.sofascore.com/api/v1/team/"
 HEADERS = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'ca,en-US;q=0.7,en;q=0.3',
-    'Connection': 'keep-alive',
-    'Host': 'api.sofascore.com',
-    'If-None-Match': 'W/"db2547deaf"',
-    'Sec-Fetch-Dest': 'document',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'none',
-    'Sec-Fetch-User': '?1',
-    'Upgrade-Insecure-Requests': "1",
-    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0'
+    'accept': '*/*',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'ca-ES,ca;q=0.9,es;q=0.8,en;q=0.7',
+    'authority': 'api.sofascore.com',
+    'cache-control': 'max-age=0',
+    'if-none-match': 'W/"d9193d7c2e"',
+    'origin': 'https://www.sofascore.com',
+    'referer': 'https://www.sofascore.com',
+    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="98"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Linux"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36'
 }
 
-'''
-    "authority": "api.sofascore.com",
-    "cache-control": "max-age=0",
-    "sec-ch-ua": "^\^Chromium^^;v=^\^92^^, ^\^"
-
-{
-        'Host':'d.flashscore.com',
-        'User-Agent':'core',
-        'Accept':'*/*',
-        'Accept-Language':'*',
-        'Accept-Encoding':'gzip,deflate,br',
-        'Referer':'https://d.flashscore.com/x/feed/proxy-local',
-        'X-GeoIP':'1',
-        'X-Referer':'https://www.flashscore.com/tennis/',
-        'X-Fsign':'SW9D1eZo',
-        'X-Requested-With':'XMLHttpRequest',
-        'Connection': 'keep-alive',
-        'Cookie':'_ga=GA1.2.149667040.1559363495; _gid=GA1.2.1231578565.1559363495; _sessionhits_UA-207011-5=2; _gat_UA-207011-5=1; _session_UA-207011-5=true',
-        'TE':'Trailers'
-}
-'''
+def getJSONFromURL(url):
+    return json.loads(requests.request("GET", url, data = "", headers = HEADERS).text)
 
 def getPlayers(fromID = 1, toID = None):
     currentID = fromID
@@ -47,12 +32,8 @@ def getPlayers(fromID = 1, toID = None):
 
     while not end:
         url = "{}{}".format(BASE_URL, currentID)
-        soup = requests.request("GET", url, data = "", headers = HEADERS).text
-        print(soup)
-        exit()
-        dataJSON = json.loads(urllib.request.urlopen(url).read())
+        dataJSON = getJSONFromURL(url)
         printCollection(dataJSON)
-        exit()
         currentID += 1
 
 getPlayers(1, 100)
