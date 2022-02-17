cb217c952bc3744db514884df6ae5ba05ed87c22
juxtecorp <xaviergs1984@gmail.com>
2022-02-17 06:56:14 +0100

    Better get players control

--- sofaScore.py
+++ sofaScore.py
@@ -8,25 +8,30 @@ from utils import *
 
 BASE_URL = "https://api.sofascore.com/api/v1/team/"
 HEADERS = {
-    'accept': '*/*',
-    'accept-encoding': 'gzip, deflate, br',
-    'accept-language': 'ca-ES,ca;q=0.9,es;q=0.8,en;q=0.7',
-    'authority': 'api.sofascore.com',
-    'cache-control': 'max-age=0',
-    'if-none-match': 'W/"d9193d7c2e"',
-    'origin': 'https://www.sofascore.com',
-    'referer': 'https://www.sofascore.com',
-    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="98"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Linux"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-site',
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36'
+    "cache-control": "max-age=0",
+    "sec-ch-ua": '"Google Chrome";v="93", " Not;A Brand";v="99", "Chromium";v="93"',
+    "sec-ch-ua-mobile": "?0",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "accept": "*/*",
+    "origin": "https://www.sofascore.com",
+    "sec-fetch-site": "same-site",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-dest": "empty",
+    "referer": "https://www.sofascore.com/",
+    #    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7"
 }
 
 def getJSONFromURL(url):
-    return json.loads(requests.request("GET", url, data = "", headers = HEADERS).text)
+    content = requests.request("GET", url, data = "", headers = HEADERS).text
+    print(content)
+
+    try:
+        jsonContent = json.loads(content)
+        return jsonContent
+    except:
+        return False
 
 def getPlayers(fromID = 1, toID = None):
     seed(1)
@@ -37,12 +42,14 @@ def getPlayers(fromID = 1, toID = None):
         url = "{}{}".format(BASE_URL, currentID)
         dataJSON = getJSONFromURL(url)
         
-        if dataJSON['team']['category']['sport']['id'] == 5:
-            print(dataJSON)
-            exit()
+        if dataJSON:
+            print("PLAYER!!!")
+
+            if not "error" in dataJSON and "team" in dataJSON and dataJSON['team']['category']['sport']['id'] == 5:
+                print(dataJSON)
 
         currentID += 1
         secondsToWait = randint(10, 15)
         time.sleep(secondsToWait)
 
-getPlayers(11, 100)
+getPlayers(2857, 6500)
