adca217e55f142ef3cce3336df231ed959a02d41
juxtecorp <xaviergs1984@gmail.com>
2022-03-20 18:33:35 +0100

    Connect MongoDB without certificate

--- models/db.py
+++ models/db.py
@@ -1,10 +1,11 @@
 from credentials import *
 from pymongo import MongoClient
+import ssl
 
 class Database:
 
     def connect(self):
-        self.connection = MongoClient(MONGODB_CONNECTION)
+        self.connection = MongoClient(MONGODB_CONNECTION, ssl_cert_reqs=ssl.CERT_NONE)
         database = self.connection['breaksDB']
         return database
     
