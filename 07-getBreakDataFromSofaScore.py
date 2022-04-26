7663794d782485047864f2bc1c0ab4c7a4a5313f
xavigs <xaviergs1984@gmail.com>
2022-04-26 13:05:19 +0200

    Save missing players when we analyze Sofa Score

--- 07-getBreakDataFromSofaScore.py
+++ 07-getBreakDataFromSofaScore.py
@@ -7,6 +7,7 @@ from models import db, objects
 dbConnection = db.Database()
 breaksDB = dbConnection.connect()
 playersObj = objects.Players(breaksDB)
+playersMissingObj = objects.PlayersMissing(breaksDB)
 
 @click.command()
 @click.option(
@@ -26,32 +27,43 @@ def getBreakDataFromSofaScore(sex, from_player, limit_player):
     players = playersObj.find_all([{'sex': sex}, {'definedGames': {"$lt": 8}}, {'startingRanking': {"$gt": from_player}}, {'startingRanking': {'$lte': limit_player }}])
     
     for player in players:
-        rankingNameLength = len(str(player['startingRanking'])) + len(player['tennisExplorerName'])
-        print("\n" + "-" * (rankingNameLength + 25))
-        print("|          ({}) {}          |".format(player['startingRanking'], player['tennisExplorerName'].upper()))
-        print("-" * (rankingNameLength + 25))
-        lastGames = {'definedGames': player['definedGames'], 'games': []}
-
-        for game in player['lastGames']:
-            previousGame = {}
-            opponent = playersObj.find([{'_id': game['opponent']}])
-
-            if opponent is None:
-                print("❌ The opponent {} is not into the database.".format(game['opponent']))
-                exit()
-            elif "sofaScoreID" in opponent:
-                previousGame['opponent'] = opponent['sofaScoreID']
-            else:
-                print("⚠️  The opponent {} does not have sofaScoreID.".format(game['opponent']))
-
-            previousGame['date'] = game['time']
-            previousGame['breakDone'] = game['breakDone']
-            previousGame['breakReceived'] = game['breakReceived']
-            lastGames['games'].append(previousGame)
+        playerMissingDB = playersMissingObj.find([{'sex': sex}, {'playerRanking': player['startingRanking']}])
         
-        lastGamesBreaks = sofaScore.checkBreaksUndefinedGamesByPlayer(player['sofaScoreID'], lastGames)
-        playersObj.updateBreakData(player['_id'], lastGamesBreaks)
-        playersObj.printBreakData(player['_id'])
+        if playerMissingDB is not None:
+            rankingNameLength = len(str(player['startingRanking'])) + len(player['tennisExplorerName'])
+            print("\n" + "-" * (rankingNameLength + 25))
+            print("|          ({}) {}          |".format(player['startingRanking'], player['tennisExplorerName'].upper()))
+            print("-" * (rankingNameLength + 25))
+            lastGames = {'definedGames': player['definedGames'], 'games': []}
+            error = False
+
+            for game in player['lastGames']:
+                previousGame = {}
+                opponent = playersObj.find([{'_id': game['opponent']}])
+
+                if opponent is None:
+                    playerMissing = {
+                        'sex': sex,
+                        'opponent': game['opponent'],
+                        'player': player['tennisExplorerName'],
+                        'playerRanking': player['startingRanking']
+                    }
+                    playersMissingObj.create(playerMissing)
+                    error = True
+                elif "sofaScoreID" in opponent:
+                    previousGame['opponent'] = opponent['sofaScoreID']
+                else:
+                    print("⚠️  The opponent {} does not have sofaScoreID.".format(game['opponent']))
+
+                previousGame['date'] = game['time']
+                previousGame['breakDone'] = game['breakDone']
+                previousGame['breakReceived'] = game['breakReceived']
+                lastGames['games'].append(previousGame)
+            
+            if not error:
+                lastGamesBreaks = sofaScore.checkBreaksUndefinedGamesByPlayer(player['sofaScoreID'], lastGames)
+                playersObj.updateBreakData(player['_id'], lastGamesBreaks)
+                playersObj.printBreakData(player['_id'])
 
 if __name__ == '__main__':
     getBreakDataFromSofaScore()
