d71daf7f03443ab79ce46bdc5028834b31e45f0d
juxtecorp <xaviergs1984@gmail.com>
2023-02-05 10:05:41 +0100

    Add conditions to get players from different positions

--- 02-getFlashScorePlayers.py
+++ 02-getFlashScorePlayers.py
@@ -14,8 +14,25 @@ position = 1
 for player in ranking:
     del(player['ranking'])
     
-    if position > 1250:
-        playersObj.update(player, [{'startingRanking': position}, {'sex': 'W'}])
+    if position > 1323:
+        if not playersObj.update(player, [{'startingRanking': position}, {'sex': 'W'}]):
+            if not playersObj.update(player, [{'startingRanking': position - 1}, {'sex': 'W'}]):
+                if not playersObj.update(player, [{'startingRanking': position - 2}, {'sex': 'W'}]):
+                    if not playersObj.update(player, [{'startingRanking': position - 3}, {'sex': 'W'}]):
+                        if not playersObj.update(player, [{'startingRanking': position - 4}, {'sex': 'W'}]):
+                            if not playersObj.update(player, [{'startingRanking': position - 21}, {'sex': 'W'}]):
+                                if not playersObj.update(player, [{'startingRanking': position - 5}, {'sex': 'W'}]):
+                                    if not playersObj.update(player, [{'startingRanking': position - 25}, {'sex': 'W'}]):
+                                        if not playersObj.update(player, [{'startingRanking': position - 6}, {'sex': 'W'}]):
+                                            if not playersObj.update(player, [{'startingRanking': position - 7}, {'sex': 'W'}]):
+                                                if not playersObj.update(player, [{'startingRanking': position - 17}, {'sex': 'W'}]):
+                                                    if not playersObj.update(player, [{'startingRanking': position - 9}, {'sex': 'W'}]):
+                                                        if not playersObj.update(player, [{'startingRanking': position - 19}, {'sex': 'W'}]):
+                                                            if not playersObj.update(player, [{'startingRanking': position - 14}, {'sex': 'W'}]):
+                                                                if not playersObj.update(player, [{'startingRanking': position - 10}, {'sex': 'W'}]):
+                                                                    if not playersObj.update(player, [{'startingRanking': position - 11}, {'sex': 'W'}]):
+                                                                        if not playersObj.update(player, [{'startingRanking': position - 8}, {'sex': 'W'}]):
+                                                                            exit()
     
     position += 1
 
