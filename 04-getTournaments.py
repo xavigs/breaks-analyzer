812264dea426814d6b1c52e34101bd34b7898157
juxtecorp <xaviergs1984@gmail.com>
2024-02-18 00:58:50 +0100

    Add ITF Tournaments

--- 04-getTournaments.py
+++ 04-getTournaments.py
@@ -3,6 +3,7 @@ from datetime import date
 import click
 from utils import *
 import tennisExplorer
+import itfTennis
 from models import db, objects
 
 dbConnection = db.Database()
@@ -20,11 +21,13 @@ tournamentsObj = objects.Tournaments(breaksDB)
 )
 
 def getYearlyTournaments(sex, year):
-    tournamentsObj.delete([{'sex': sex, 'year': year}])
+    tournamentsObj.delete([{'sex': sex}])
     print("# Getting tournaments from Tennis Explorer...")
     tournaments = tennisExplorer.getTournaments(sex, year)
+    print("# Getting tournaments from ITF Tennis...")
+    tournaments += itfTennis.getTournaments(sex, year)
     print("# Inserting tournaments into the DB...")
-    
+
     for tournament in tournaments:
         tournamentsObj.create(tournament)
 
