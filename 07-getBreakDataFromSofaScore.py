adc939c20eecc192704b1a0c96e3fba8a0ed17e0
juxtecorp <xaviergs1984@gmail.com>
2024-02-19 21:07:20 +0100

    2 retries for cursor

--- 07-getBreakDataFromSofaScore.py
+++ 07-getBreakDataFromSofaScore.py
@@ -31,58 +31,65 @@ def getBreakDataFromSofaScore(sex, from_player, limit_player):
     elif from_player > 0 and limit_player < 999999:
         players = playersObj.find_all([{'sex': sex}, {'definedGames': {"$lt": 8}}, {'startingRanking': {"$gt": from_player}}, {'startingRanking': {'$lte': limit_player }}])
 
-    for player in players:
-        playerMissingDB = playersMissingObj.find([{'sex': sex}, {'playerRanking': player['startingRanking']}])
+    for i in range(3):
+        try:
+            for player in players:
+                playerMissingDB = playersMissingObj.find([{'sex': sex}, {'playerRanking': player['startingRanking']}])
 
-        if playerMissingDB is None:
-            rankingNameLength = len(str(player['startingRanking'])) + len(player['tennisExplorerName'])
-            print("\n" + "-" * (rankingNameLength + 25))
+                if playerMissingDB is None:
+                    rankingNameLength = len(str(player['startingRanking'])) + len(player['tennisExplorerName'])
+                    print("\n" + "-" * (rankingNameLength + 25))
 
-            if "flashScoreName" in player and player['flashScoreName'] != "":
-                playerName = player['flashScoreName']
-            else:
-                playerName = player['tennisExplorerName']
+                    if "flashScoreName" in player and player['flashScoreName'] != "":
+                        playerName = player['flashScoreName']
+                    else:
+                        playerName = player['tennisExplorerName']
 
-            print(u"|          ({}) {}          |".format(player['startingRanking'], playerName.upper()))
-            print("-" * (rankingNameLength + 25))
-            lastGames = {'definedGames': player['definedGames'], 'games': []}
-            error = False
+                    print(u"|          ({}) {}          |".format(player['startingRanking'], playerName.upper()))
+                    print("-" * (rankingNameLength + 25))
+                    lastGames = {'definedGames': player['definedGames'], 'games': []}
+                    error = False
 
-            for game in player['lastGames']:
-                previousGame = {}
-                opponent = playersObj.find([{'_id': game['opponent']}])
+                    for game in player['lastGames']:
+                        previousGame = {}
+                        opponent = playersObj.find([{'_id': game['opponent']}])
 
-                if opponent is None:
-                    playerMissing = {
-                        'sex': sex,
-                        'opponent': game['opponent'],
-                        'player': player['tennisExplorerName'],
-                        'playerRanking': player['startingRanking']
-                    }
-                    playersMissingObj.create(playerMissing)
-                    error = True
-                elif "sofaScoreID" in opponent:
-                    previousGame['opponent'] = opponent['sofaScoreID']
-                else:
-                    playerMissing = {
-                        'sex': sex,
-                        'opponent': game['opponent'],
-                        'player': player['tennisExplorerName'],
-                        'playerRanking': player['startingRanking'],
-                        'sofaScoreID': opponent['startingRanking']
-                    }
-                    playersMissingObj.create(playerMissing)
-                    print("⚠️  The opponent {} does not have sofaScoreID.".format(game['opponent']))
+                        if opponent is None:
+                            playerMissing = {
+                                'sex': sex,
+                                'opponent': game['opponent'],
+                                'player': player['tennisExplorerName'],
+                                'playerRanking': player['startingRanking']
+                            }
+                            playersMissingObj.create(playerMissing)
+                            error = True
+                        elif "sofaScoreID" in opponent:
+                            previousGame['opponent'] = opponent['sofaScoreID']
+                        else:
+                            playerMissing = {
+                                'sex': sex,
+                                'opponent': game['opponent'],
+                                'player': player['tennisExplorerName'],
+                                'playerRanking': player['startingRanking'],
+                                'sofaScoreID': opponent['startingRanking']
+                            }
+                            playersMissingObj.create(playerMissing)
+                            print("⚠️  The opponent {} does not have sofaScoreID.".format(game['opponent']))
 
-                previousGame['date'] = game['time']
-                previousGame['breakDone'] = game['breakDone']
-                previousGame['breakReceived'] = game['breakReceived']
-                lastGames['games'].append(previousGame)
+                        previousGame['date'] = game['time']
+                        previousGame['breakDone'] = game['breakDone']
+                        previousGame['breakReceived'] = game['breakReceived']
+                        lastGames['games'].append(previousGame)
 
-            if not error:
-                lastGamesBreaks = sofaScore.checkBreaksUndefinedGamesByPlayer(player['sofaScoreID'], lastGames)
-                playersObj.updateBreakData(player['_id'], lastGamesBreaks)
-                playersObj.printBreakData(player['_id'])
+                    if not error:
+                        lastGamesBreaks = sofaScore.checkBreaksUndefinedGamesByPlayer(player['sofaScoreID'], lastGames)
+                        playersObj.updateBreakData(player['_id'], lastGamesBreaks)
+                        playersObj.printBreakData(player['_id'])
+            break
+        except:
+            if i == 2:
+                raise
+            continue
 
 if __name__ == '__main__':
     getBreakDataFromSofaScore()
