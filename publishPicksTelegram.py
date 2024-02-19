e105a76b9bbde817d9e70a45c5854282762d3aee
juxtecorp <xaviergs1984@gmail.com>
2024-02-19 19:24:55 +0100

    Send picture to Telegram

--- publishPicksTelegram.py
+++ publishPicksTelegram.py
@@ -17,17 +17,19 @@ tournamentsObj = objects.Tournaments(breaksDB)
 TOKEN = '6976497176:AAEL9sNN9ysFrycNN1TmOS0Z68pAyqIzAYU'
 bot = telegram.Bot(token=TOKEN)
 channelID = '-1001505314231'
+#channelID = '222946111'
 tipsterlandID = '16775'
 
 # Constants and variables
 emojisNumbers = ('0️⃣', '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣')
 weekdays = ('Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo')
+SURFACES = {'I': 'Pista dura indoor', 'D': 'Pista dura outdoor', 'T': 'Tierra batida', 'H': 'Hierba', 'M': 'Moqueta'}
 FLAGS = {
     'germany': '🇩🇪'
 }
 
-async def send(message):
-    await bot.send_message(chat_id=channelID, text=message, parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)
+async def send(imageURL, message):
+    await bot.send_photo(chat_id=channelID, photo=imageURL, caption=message, parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)
 
 # Get Tipsterland picks
 picksTelegramDB = picksTelegramObj.find_all()
@@ -35,7 +37,7 @@ numPublishedPicks = len(list(picksTelegramDB))
 url = 'https://www.tipsterland.com/api/picks/cards?tipster_id={}&month=2&year=2024&pending=false&requestId=1&page=1'.format(tipsterlandID)
 jsonContent = json.loads(requests.get(url).text)
 soup = BeautifulSoup(jsonContent['data'], 'lxml')
-picks = soup.select('div[class=card-body]')
+picks = soup.select('div[class*=x-pick-card]')
 today = datetime.now()
 numPicks = 0
 numPick = numPublishedPicks + 1
@@ -50,6 +52,7 @@ for pick in picks:
     pickDate = pickDate.replace(year=today.year)
 
     if pickDate > today or numPicks < 1:
+        # Future or first pick
         pickDB = {}
         numPickString = str(numPick)
         numPickEmoji = ''
@@ -79,9 +82,17 @@ for pick in picks:
         foundPick = picksTelegramObj.find([{'date': pickDate.strftime('%Y-%m-%d %H:%M')}, {'pick': pickDB['pick']}])
 
         if foundPick is None:
-            message = '{} *{}*\n\n🏆 {} {}\n🎾 {}\n⏰ {}\n💰 @{}'.format(numPickEmoji, re.escape(pickDB['pick']), pickDB['competition'], flag, pickDB['event'], re.escape(pickDB['date']), pickDB['odd'])
+            # Get picture
+            pickID = pick.select_one('button[data-pick-id]')['data-pick-id']
+            pickURL = 'https://www.tipsterland.com/api/dlg-pick-view/{}?dlgId=dlg_pick_card_details&show_tipster=false'.format(pickID)
+            r = requests.get(pickURL)
+            pickSoup = BeautifulSoup(r.text, 'lxml')
+            imageURL = pickSoup.select_one('img[class=pick-image]')['src']
+
+            # Send message to Telegram
+            message = '{} *{}*\n\n🏆 {} {}\n📌 {}\n🎾 {}\n⏰ {}\n💰 @{}'.format(numPickEmoji, re.escape(pickDB['pick']), pickDB['competition'], flag, SURFACES[tournament['surface']], pickDB['event'], re.escape(pickDB['date']), pickDB['odd'])
             print(message)
-            asyncio.run(send(message))
+            asyncio.run(send(imageURL, message))
 
             # Insert into DB
             pickTelegram = {
